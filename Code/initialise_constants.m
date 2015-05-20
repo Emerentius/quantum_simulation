3aed55323aeb41bd5f5de1e2748c6d785fa3fe69
Emerentius <Emerentius@arcor.de>
2015-05-21 01:22:28 +0200

    centralized definition of physical constants

---
+++ Code/initialise_constants.m
@@ -0,0 +1,30 @@
+% all SI units
+
+% mathematical
+
+% fundamental
+c = 299792458;
+h = 6.62606957e-34;
+h_bar = 1.054571726e-34;
+
+% nuclear, atomic
+m_e = 9.10938291e-31;
+m_p = 1.672621777e-27;
+
+% physico-chemical
+k_B = 1.3806488e-23;
+k_b = k_B;
+N_A = 6.02214129e23;
+
+% electromagnetic
+mu_0 = 1.256637061e-6;
+eps_0 = 8.854187817e-12;
+e = 1.602176565e-19;
+mu_bohr = 9.27400968e-24;
+
+% helper
+eV = e;
+eV_per_J = 1/e;
+J_per_eV = eV;
+
+nm = 1e-9;
