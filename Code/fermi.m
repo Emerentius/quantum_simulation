d3b9b060e1f7107c2a43b61ce06f18459e05f092
Emerentius <Emerentius@arcor.de>
2015-06-23 18:35:22 +0200

    Fermi function special case
    Added special case to allow for zero temperature

--- Code/fermi.m
+++ Code/fermi.m
@@ -1,4 +1,10 @@
 function output = fermi(E, T)
     k_B = 1.38e-23;
-    output = 1/(exp(E/(k_B*T)) + 1);
+    if E == 0 
+       % this is necessary, because the calculation below breaks down
+       % for T = 0 and returns NaN.
+       output = 0.5;
+    else
+       output = 1/(exp(E/(k_B*T)) + 1);
+    end
 end
