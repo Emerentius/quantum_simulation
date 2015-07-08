863af895621d0ded7ab0b1813df06a9619b9410a
Emerentius <Emerentius@arcor.de>
2015-07-08 19:22:28 +0200

    homework 6
    The comparison homework. The result is correct.

---
+++ Code/HA_6_17.m
@@ -0,0 +1,50 @@
+%clear all;
+%close all;
+%profile on
+
+import Transistor.Transistor;
+initialise_constants
+%% input parameters and transistor instantiation
+tr = Transistor(0.5, ...    % V_ds
+                0.0, ...    % V_g
+                3,   ...    % d_ch
+                3,   ...    % d_ox
+                0.5, ...    % a
+                'm',         0.2*m_e,      ...
+                'E_f',       0.15,         ... % eV
+                'l_ch',      25,           ...
+                'dE',        5e-4,         ...
+                'T',         300,          ...
+                'eps_ch',    eps_si,       ...
+                'eps_ox',    eps_sio2,     ...
+                'l_ds',      '7 lambda',   ...
+                'E_g',       1,            ... % eV
+                'lambda_ds', '1 lambda', ... % multiple of lambda_ch
+                'geometry',  'nano-wire');
+
+% step size of newton raphson
+NEWTON_STEP_SIZE = 0.3; % of full delta phi
+LIMIT_DELTA_PHI = 1e-3;
+MAX_ITERATIONS = 200;
+
+%%
+V_g_range = 0.0: 0.05 : 0.7;
+phi_fig = figure;
+hold on;
+for Vgjj = [V_g_range; 1:length(V_g_range)]
+    V_g = Vgjj(1);
+    jj = Vgjj(2);
+    tr.set_V_g(V_g);
+    disp(tr.make_self_consistent(NEWTON_STEP_SIZE, LIMIT_DELTA_PHI, MAX_ITERATIONS));
+    I(jj) = tr.current;
+    if V_g >= 0.5 
+        tr.plot_phi(phi_fig)
+    end
+end
+figure
+plot(V_g_range, I);
+xlabel('V_g');
+ylabel('$\log_{10}\left(\frac{I}{A}\right))$', 'interpreter', 'latex');
+
+%profile off
+%profile viewer
